be7ec00ef5a016bb8774d7978734a433e4fc68c1
ShahidBinShafiq <shahidvital786@gmail.com>
2024-09-28 22:56:51 +0500

    updated

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// let header = document.querySelector("#header");
+// let heroSect = document.querySelector("#heroSect");
+// let header_height = header.offsetHeight;
+// heroSect.style.paddingTop = header_height + "px";
+
+
+
+
